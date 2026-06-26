ae7f42ea4b2eac6d93097aa21ddfc0f3ce95fac3
nedarctic <nedarctic@gmail.com>
2026-06-26 16:50:01 +0300

    r2 env variables

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,4 +1,9 @@
 export default () => ({
     DATABASE_URL: process.env.DATABASE_URL,
     JWT_SECRET: process.env.JWT_SECRET,
+    R2_ACCESS_KEY_ID: process.env.R2_ACCESS_KEY_ID,
+    R2_SECRET_ACCESS_KEY: process.env.R2_SECRET_ACCESS_KEY,
+    R2_ACCOUNT_ID: process.env.R2_ACCOUNT_ID,
+    R2_BUCKET_NAME: process.env.R2_BUCKET_NAME,
+    R2_PUBLIC_URL: process.env.R2_PUBLIC_URL,
 });
--- src/config/validation.ts
+++ src/config/validation.ts
@@ -3,4 +3,8 @@ import * as Joi from 'joi';
 export const validationSchema = Joi.object({
     DATABASE_URL: Joi.string().required(),
     JWT_SECRET: Joi.string().required(),
+    R2_ACCESS_KEY_ID: Joi.string().required(),
+    R2_SECRET_ACCESS_KEY: Joi.string().required(),
+    R2_ACCOUNT_ID: Joi.string().required(),
+    R2_BUCKET_NAME: Joi.string().required(),
 });
