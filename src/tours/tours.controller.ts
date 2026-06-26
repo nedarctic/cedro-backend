9e02b1d25ac067610d2286f0dacb9280185290ba
nedarctic <nedarctic@gmail.com>
2026-06-26 15:58:39 +0300

    api security for tours

--- src/tours/tours.controller.ts
+++ src/tours/tours.controller.ts
@@ -1,21 +1,27 @@
-import { Controller, Post, Get, Param, Body, Patch, Delete, Query, Logger, UseFilters } from '@nestjs/common';
+import { Controller, Post, Get, Param, Body, Patch, Delete, Query, Logger, UseFilters, UseGuards } from '@nestjs/common';
 import { ToursService } from './tours.service';
 import { CreateTourDto } from './dto/create-tour.dto';
 import { PaginationDto } from '../common/dtos/pagination.dto';
 import { UpdateTourDto } from './dto/update-tour.dto';
 import { ResponseFilter } from '../common/filters/reponse.filter';
+import { JwtAuthGuard } from '../auth/guards/jwt.guard';
+import { RoleGuard } from '../auth/guards/role.guard';
+import { Roles } from '../auth/decorators/role.decorator';
+import { UserRole } from '../generated/prisma/enums';
 
 @UseFilters(ResponseFilter)
 @Controller('tours')
 export class ToursController {
 
     private readonly logger = new Logger(ToursController.name);
-    
+
     constructor(
         private readonly tours: ToursService
     ) { }
 
     // create a tour
+    @UseGuards(JwtAuthGuard, RoleGuard)
+    @Roles(UserRole.SUPER_ADMIN)
     @Post()
     async createTour(@Body() dto: CreateTourDto) {
         return await this.tours.createTour(dto);
@@ -35,12 +41,16 @@ export class ToursController {
     }
 
     // update a tour
+    @UseGuards(JwtAuthGuard, RoleGuard)
+    @Roles(UserRole.SUPER_ADMIN)
     @Patch(':tourId')
     async updateTour(@Param('tourId') tourId: string, @Body() dto: UpdateTourDto) {
         return await this.tours.updateTour(tourId, dto);
     }
 
     // delete a tour
+    @UseGuards(JwtAuthGuard, RoleGuard)
+    @Roles(UserRole.SUPER_ADMIN)
     @Delete(':tourId')
     async deleteTour(@Param('tourId') tourId: string) {
         return await this.tours.deleteTour(tourId);
