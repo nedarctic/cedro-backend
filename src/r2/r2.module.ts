8a043ba35d70f721cdfc35b1700f7fbd633039b0
nedarctic <nedarctic@gmail.com>
2026-06-28 11:27:55 +0300

    export r2service from mod

--- src/r2/r2.module.ts
+++ src/r2/r2.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { R2Service } from './r2.service';
 
 @Module({
-  providers: [R2Service]
+  providers: [R2Service],
+  exports: [R2Service]
 })
-export class R2Module {}
+export class R2Module { }
