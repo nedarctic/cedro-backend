f9d09a83f78b5ee5dc3f457739b1494393d9ca12
nedarctic <nedarctic@gmail.com>
2026-06-29 16:37:05 +0300

    auth guard for user api

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,14 @@
-import { Controller, Post, Get, Query, Param, Body, Patch, Delete, UseFilters } from '@nestjs/common';
+import { Controller, Post, Get, Query, Param, Body, Patch, Delete, UseFilters, UseGuards } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { PaginationDto } from '../common/dtos/pagination.dto';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { UsersService } from './users.service';
 import { ResponseFilter } from '../common/filters/reponse.filter';
+import { JwtAuthGuard } from '../auth/guards/jwt.guard';
+import { RoleGuard } from '../auth/guards/role.guard';
+import { Roles } from '../auth/decorators/role.decorator';
+import { UserRole } from '../generated/prisma/enums';
 
 @UseFilters(ResponseFilter)
 @Controller('users')
@@ -21,6 +25,8 @@ export class UsersController {
     }
 
     // create a user
+    @UseGuards(JwtAuthGuard, RoleGuard)
+    @Roles(UserRole.SUPER_ADMIN)
     @Post()
     async createUser(@Body() dto: CreateUserDto) {
         return await this.users.createUser(dto);
@@ -33,12 +39,16 @@ export class UsersController {
     }
 
     // update user by userId
+    @UseGuards(JwtAuthGuard, RoleGuard)
+    @Roles(UserRole.SUPER_ADMIN)
     @Patch(':userId')
     async updateUser(@Param('userId') userId: string, @Body() dto: UpdateUserDto) {
         return await this.users.updateUser(userId, dto)
     }
 
     // delete user by userId
+    @UseGuards(JwtAuthGuard, RoleGuard)
+    @Roles(UserRole.SUPER_ADMIN)
     @Delete(':userId')
     async deleteUser(@Param('userId') userId: string) {
         return await this.users.deleteUser(userId);
