4113fe30979cec215364a62f52324589b1b13ed4
nedarctic <nedarctic@gmail.com>
2026-06-29 16:39:29 +0300

    itineraries controllers

--- src/itineraries/itineraries.controller.ts
+++ src/itineraries/itineraries.controller.ts
@@ -1,4 +1,57 @@
-import { Controller } from '@nestjs/common';
+import {
+    Controller,
+    Get,
+    Post,
+    Patch,
+    Delete,
+    Param,
+    UseInterceptors,
+    UploadedFile,
+    Body,
+    UseGuards
+} from '@nestjs/common';
+import { ItinerariesService } from './itineraries.service';
+import { CreateItineraryDto } from './dto/create-itinerary.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { JwtAuthGuard } from '../auth/guards/jwt.guard';
+import { RoleGuard } from '../auth/guards/role.guard';
+import { Roles } from '../auth/decorators/role.decorator';
+import { UserRole } from '../generated/prisma/enums';
 
 @Controller('itineraries')
-export class ItinerariesController {}
+export class ItinerariesController {
+    constructor(
+        private readonly itineraries: ItinerariesService
+    ) { }
+
+    @UseGuards(JwtAuthGuard, RoleGuard)
+    @Roles(UserRole.SUPER_ADMIN)
+    @UseInterceptors(FileInterceptor('itineraryImage'))
+    @Post(':tourId')
+    async createItinerary(
+        @Param('tourId') tourId: string,
+        @Body() dto: CreateItineraryDto,
+        @UploadedFile() itineraryImage: Express.Multer.File
+    ) {
+        return await this.itineraries.createItinerary(tourId, dto, itineraryImage);
+    }
+
+    @Get(':tourId')
+    async getTourItineraries(@Param('tourId') tourId: string) {
+        return await this.itineraries.getItineraries(tourId);
+    }
+
+    @UseGuards(JwtAuthGuard, RoleGuard)
+    @Roles(UserRole.SUPER_ADMIN)
+    @Patch(':itineraryId')
+    async updateItinerary(@Param('itineraryId') itineraryId: string) {
+        return await this.updateItinerary(itineraryId);
+    }
+
+    @UseGuards(JwtAuthGuard, RoleGuard)
+    @Roles(UserRole.SUPER_ADMIN)
+    @Delete(':itineraryId')
+    async deleteItinerary(@Param('itineraryId') itineraryId: string) {
+        return await this.itineraries.deleteItinerary(itineraryId);
+    }
+}
