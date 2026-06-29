4fdc8f3f54c5aef9aa7b201595768a3aaa4c0cde
nedarctic <nedarctic@gmail.com>
2026-06-29 16:38:13 +0300

    destinations controllers

--- src/destinations/destinations.controller.ts
+++ src/destinations/destinations.controller.ts
@@ -1,4 +1,48 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, UseGuards } from '@nestjs/common';
+import { PaginationDto } from '../common/dtos/pagination.dto';
+import { DestinationsService } from './destinations.service';
+import { JwtAuthGuard } from '../auth/guards/jwt.guard';
+import { RoleGuard } from '../auth/guards/role.guard';
+import { Roles } from '../auth/decorators/role.decorator';
+import { UserRole } from '../generated/prisma/enums';
 
 @Controller('destinations')
-export class DestinationsController {}
+export class DestinationsController {
+    constructor(
+        private readonly destinations: DestinationsService
+    ) { }
+
+    @UseGuards(JwtAuthGuard, RoleGuard)
+    @Roles(UserRole.SUPER_ADMIN)
+    @Post()
+    async createDestination(@Body() dto: { name: string }) {
+        return await this.destinations.createDestination(dto.name);
+    }
+
+    @Get()
+    async getDestinations(@Body() dto: PaginationDto) {
+        return await this.destinations.getDestinations(dto);
+    }
+
+    @Get(':destinationId')
+    async getDestination(@Param('destinationId') destinationId: string) {
+        return await this.destinations.getDestination(destinationId);
+    }
+
+    @UseGuards(JwtAuthGuard, RoleGuard)
+    @Roles(UserRole.SUPER_ADMIN)
+    @Patch(':destinationId')
+    async updateDestination(
+        @Param('destinationId') destinationId: string,
+        @Body() dto: { name: string }
+    ) {
+        return await this.destinations.updateDestination(destinationId, dto.name);
+    }
+
+    @UseGuards(JwtAuthGuard, RoleGuard)
+    @Roles(UserRole.SUPER_ADMIN)
+    @Delete(':destinationId')
+    async deleteDestination(@Param('destinationId') destinationId: string) {
+        return await this.destinations.deleteDestination(destinationId);
+    }
+}
