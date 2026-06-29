19d6d1d1c5f2975c3caf1ec5467c73267084caaf
nedarctic <nedarctic@gmail.com>
2026-06-29 16:42:11 +0300

    tour update dto update

--- src/tours/dto/update-tour.dto.ts
+++ src/tours/dto/update-tour.dto.ts
@@ -1,7 +1,48 @@
-import { IsOptional, IsString } from "class-validator";
+import { ArrayMinSize, IsArray, IsNotEmpty, IsOptional, IsString } from "class-validator";
 
 export class UpdateTourDto {
     @IsOptional()
     @IsString()
     title?: string;
+
+    @IsOptional()
+    @IsString()
+    description!: string;
+
+    @IsOptional()
+    @IsArray()
+    @ArrayMinSize(1)
+    @IsString({ each: true })
+    @IsNotEmpty({ each: true })
+    activities!: string[];
+
+    @IsOptional()
+    @IsArray()
+    @ArrayMinSize(1)
+    @IsString({ each: true })
+    @IsNotEmpty({ each: true })
+    included!: string[];
+
+    @IsOptional()
+    @IsArray()
+    @ArrayMinSize(1)
+    @IsString({ each: true })
+    @IsNotEmpty({ each: true })
+    excluded!: string[];
+
+    @IsOptional()
+    @IsString()
+    dates!: string;
+
+    @IsOptional()
+    @IsString()
+    groupSize!: string;
+
+    @IsOptional()
+    @IsString()
+    price!: string;
+
+    @IsOptional()
+    @IsString()
+    duration!: string;
 }
