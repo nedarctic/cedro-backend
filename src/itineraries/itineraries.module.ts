5dff776642e43601a1b4e36e24cf03e244225b6a
nedarctic <nedarctic@gmail.com>
2026-06-29 16:39:41 +0300

    module imports

--- src/itineraries/itineraries.module.ts
+++ src/itineraries/itineraries.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ItinerariesService } from './itineraries.service';
 import { ItinerariesController } from './itineraries.controller';
+import { PrismaModule } from '../prisma/prisma.module';
+import { ToursModule } from '../tours/tours.module';
+import { R2Module } from '../r2/r2.module';
 
 @Module({
+  imports: [PrismaModule, ToursModule, R2Module],
   providers: [ItinerariesService],
   controllers: [ItinerariesController]
 })
