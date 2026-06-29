6b152ef19084044a91b997c4fbdb34f8bf6fa4ec
nedarctic <nedarctic@gmail.com>
2026-06-29 11:01:22 +0300

    dto for creating tours

--- src/tours/dto/create-tour.dto.ts
+++ src/tours/dto/create-tour.dto.ts
@@ -1,6 +1,39 @@
-import { IsString } from "class-validator";
+import { ArrayMinSize, IsArray, IsNotEmpty, IsString } from "class-validator";
 
 export class CreateTourDto {
     @IsString()
     title!: string;
+
+    @IsString()
+    description!: string;
+
+    @IsArray()
+    @ArrayMinSize(1)
+    @IsString({ each: true })
+    @IsNotEmpty({ each: true })
+    activities!: string[];
+
+    @IsArray()
+    @ArrayMinSize(1)
+    @IsString({ each: true })
+    @IsNotEmpty({ each: true })
+    included!: string[];
+
+    @IsArray()
+    @ArrayMinSize(1)
+    @IsString({ each: true })
+    @IsNotEmpty({ each: true })
+    excluded!: string[];
+
+    @IsString()
+    dates!: string;
+
+    @IsString()
+    groupSize!: string;
+
+    @IsString()
+    price!: string;
+
+    @IsString()
+    duration!: string;
 }
