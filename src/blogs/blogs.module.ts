86c56a8d9d81fade01fb232f9d582d76cc9e0a0c
nedarctic <nedarctic@gmail.com>
2026-07-01 11:55:47 +0300

    mod imports

--- src/blogs/blogs.module.ts
+++ src/blogs/blogs.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { BlogsController } from './blogs.controller';
 import { BlogsService } from './blogs.service';
+import { PrismaModule } from '../prisma/prisma.module';
+import { R2Module } from '../r2/r2.module';
 
 @Module({
+  imports: [PrismaModule, R2Module],
   controllers: [BlogsController],
   providers: [BlogsService]
 })
