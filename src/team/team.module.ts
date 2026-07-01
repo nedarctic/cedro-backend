2d5ea30dac844193941b5f3ae5607d3b1dae81da
nedarctic <nedarctic@gmail.com>
2026-07-02 01:13:36 +0300

    team mod imports

--- src/team/team.module.ts
+++ src/team/team.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TeamController } from './team.controller';
 import { TeamService } from './team.service';
+import { R2Module } from '../r2/r2.module';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
+  imports: [R2Module, PrismaModule],
   controllers: [TeamController],
   providers: [TeamService]
 })
