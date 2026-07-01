c3de08bef60669564f2aa657fb58e7492922622a
nedarctic <nedarctic@gmail.com>
2026-07-02 01:13:26 +0300

    team controllers

--- src/team/team.controller.ts
+++ src/team/team.controller.ts
@@ -1,4 +1,66 @@
-import { Controller } from '@nestjs/common';
+import {
+    Controller,
+    Get,
+    Post,
+    Patch,
+    Delete,
+    Param,
+    Query,
+    Body,
+    UploadedFile,
+    UseGuards,
+    UseInterceptors
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { JwtAuthGuard } from '../auth/guards/jwt.guard';
+import { RoleGuard } from '../auth/guards/role.guard';
+import { Roles } from '../auth/decorators/role.decorator';
+import { TeamService } from './team.service';
+import { UserRole } from '../generated/prisma/enums';
+import { CreateMemberDto } from './dto/create-member.dto';
+import { UpdateMemberDto } from './dto/update-member.dto';
 
 @Controller('team')
-export class TeamController {}
+export class TeamController {
+    constructor(
+        private readonly team: TeamService
+    ) { }
+
+    @UseGuards(JwtAuthGuard, RoleGuard)
+    @Roles(UserRole.SUPER_ADMIN)
+    @UseInterceptors(FileInterceptor('memberImage'))
+    @Post()
+    async createTour(@Body() dto: CreateMemberDto, @UploadedFile() memberImage: Express.Multer.File) {
+        return this.team.createMember(dto, memberImage)
+    }
+
+    @Get()
+    async getTeam(@Query() search: string) {
+        return await this.team.getTeam(search);
+    }
+
+    @Get(':memberId')
+    async getMember(@Param('memberId') memberId: string) {
+        return await this.getMember(memberId);
+    }
+
+    @UseGuards(JwtAuthGuard, RoleGuard)
+    @Roles(UserRole.SUPER_ADMIN)
+    @UseInterceptors(FileInterceptor('memberImage'))
+    @Patch(':memberId')
+    async updateMember(
+        @Param('memberId') memberId: string,
+        @Body() dto: UpdateMemberDto,
+        @UploadedFile() memberImage: Express.Multer.File
+    ) {
+        return await this.team.updateMember(memberId, dto, memberImage)
+    }
+
+    @UseGuards(JwtAuthGuard, RoleGuard)
+    @Roles(UserRole.SUPER_ADMIN)
+    @UseInterceptors(FileInterceptor('memberImage'))
+    @Delete(':memberId')
+    async deleteMember(@Param('memberId') memberId: string) {
+        return await this.deleteMember(memberId);
+    }
+}
