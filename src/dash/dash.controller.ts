a3e10f88996d009429bcfe88f0a84b202eab535d
nedarctic <nedarctic@gmail.com>
2026-07-14 06:33:40 +0300

    kpis get controller

--- src/dash/dash.controller.ts
+++ src/dash/dash.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, UseGuards } from '@nestjs/common';
+import { DashService } from './dash.service';
+import { JwtAuthGuard } from '../auth/guards/jwt.guard';
+import { RoleGuard } from '../auth/guards/role.guard';
+import { Roles } from '../auth/decorators/role.decorator';
+import { UserRole } from '../generated/prisma/enums';
 
+@UseGuards(JwtAuthGuard, RoleGuard)
+@Roles(UserRole.SUPER_ADMIN)
 @Controller('dash')
-export class DashController {}
+export class DashController {
+    constructor(
+        private readonly dashService: DashService
+    ){}
+
+    @Get('/kpis')
+    async getDashKpis () {
+        return await this.dashService.getDashKpis();
+    }
+}
