37229e00c43f605f41ef772a2e8ff98b667ac502
nedarctic <nedarctic@gmail.com>
2026-07-14 06:33:26 +0300

    prisma module import

--- src/dash/dash.module.ts
+++ src/dash/dash.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { DashController } from './dash.controller';
 import { DashService } from './dash.service';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [DashController],
   providers: [DashService]
 })
