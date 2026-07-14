8d6f51bff19714568564b838f70fb46c7ca89f2b
nedarctic <nedarctic@gmail.com>
2026-07-14 06:33:13 +0300

    dash kpis

--- src/dash/dash.service.ts
+++ src/dash/dash.service.ts
@@ -1,4 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
-export class DashService {}
+export class DashService {
+    constructor(
+        private readonly prisma: PrismaService
+    ){}
+
+    // get dash kpis
+    async getDashKpis () {
+        
+        const [
+            totalTours, 
+            totalBookings,
+            totalBlogs,
+            totalMembers,
+            totalDestinations
+        ] = await Promise.all([
+            await this.prisma.tour.count(),
+            await this.prisma.booking.count(),
+            await this.prisma.blog.count(),
+            await this.prisma.teamMember.count(),
+            await this.prisma.destination.count()
+        ]);
+
+        return {
+            totalTours,
+            totalBookings,
+            totalBlogs,
+            totalMembers,
+            totalDestinations
+        }
+    }
+}
